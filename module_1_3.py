a531eceebdd852916cb45fda6bc3e5b8409718c3
MirraGins <mirra1@me.com>
2024-09-17 18:08:58 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name="mirra"
+print(name)
+age=21
+print(age)
+new_age=1
+print(age+new_age)
+is_student="true"
+print(is_student)
+
+print('Name:',name)
+print('Age:',age)
+print('New age:',new_age+age)
+print('Is student:',is_student)
